d507df7745e41216ae585ffd643c23b204dc7ccd
evanbowman <ebowman@bu.edu>
2021-07-03 09:55:10 -0400

    edit challenges script

--- scripts/challenge.lisp
+++ scripts/challenge.lisp
@@ -6,29 +6,7 @@
 (eval-other-file "reset_hooks.lisp")
 
 
-
-;; Initial setup for player's island
-(configure-player
- (player)
- '((power-core 1 13)))
-
-
-(add-chr (player) 2 14)
-
-(set 'challenges-completed '())
-
-
 (gc)
 
 
-'(("1: Goliath" . "challenges/goliath.lisp")
-  ("2: The Flatiron" . "challenge-2.lisp")
-  ("3: Goblin Raid" . "challenge-3.lisp")
-  ("4: Wind Fortress" . "...")
-  ("5: " . "...")
-  ("6: " . "...")
-  ("7: " . "...")
-  ("8: " . "...")
-  ("9: " . "...")
-  ("10: " . "...")
-  ("11: " . "..."))
+'(("1: Goliath" . "challenges/goliath.lisp"))
