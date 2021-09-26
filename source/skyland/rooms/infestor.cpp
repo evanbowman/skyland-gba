019bb66d705513084f0ddeea83e4e7a9af17ca50
evanbowman <ebowman@bu.edu>
2021-09-26 11:27:20 -0400

    commit missing files

---
+++ source/skyland/rooms/infestor.cpp
@@ -0,0 +1,38 @@
+#include "infestor.hpp"
+
+
+
+namespace skyland {
+
+
+
+Infestor::Infestor(Island* parent, const Vec2<u8>& position)
+    : Room(parent, name(), size(), position, Health(200))
+{
+
+}
+
+
+
+void Infestor::update(Platform&, App&, Microseconds delta)
+{
+
+}
+
+
+
+void Infestor::render_interior(Platform& pfrm, Layer layer)
+{
+
+}
+
+
+
+void Infestor::render_exterior(Platform& pfrm, Layer layer)
+{
+
+}
+
+
+
+}
---
+++ source/skyland/rooms/infestor.hpp
@@ -0,0 +1,110 @@
+#pragma once
+
+
+#include "skyland/coins.hpp"
+#include "skyland/room.hpp"
+
+
+
+namespace skyland {
+
+
+
+class Infestor : public Room {
+public:
+    Infestor(Island* parent, const Vec2<u8>& position);
+
+
+    void update(Platform&, App&, Microseconds delta) override;
+
+
+    void render_interior(Platform& pfrm, Layer layer) override;
+    void render_exterior(Platform& pfrm, Layer layer) override;
+
+
+    bool has_roof() override
+    {
+        return false;
+    }
+
+
+    bool description_visible() override
+    {
+        return true;
+    }
+
+
+    static Vec2<u8> size()
+    {
+        return {1, 1};
+    }
+
+
+    static const char* name()
+    {
+        return "infestor";
+    }
+
+
+    static Coins cost()
+    {
+        return 1000;
+    }
+
+
+    static Float ai_base_weight()
+    {
+        return 800.f;
+    }
+
+
+    //    ScenePtr<Scene> select(Platform& pfrm, App&) override;
+
+
+    void set_target(const Vec2<u8>& target) override
+    {
+        target_ = target;
+    }
+
+
+    void unset_target() override
+    {
+        target_.reset();
+    }
+
+
+    void plot_walkable_zones(bool matrix[16][16]) override
+    {
+        // one cannot walk through this tile, intentionally do nothing.
+    }
+
+
+    static Power consumes_power()
+    {
+        return 34;
+    }
+
+
+    static Icon icon()
+    {
+        return 552;
+    }
+
+
+    static Icon unsel_icon()
+    {
+        return 536;
+    }
+
+
+private:
+    static constexpr const Microseconds reload_time = milliseconds(3500);
+
+
+    Microseconds reload_ = reload_time;
+
+    std::optional<Vec2<u8>> target_;
+};
+
+
+} // namespace skyland
