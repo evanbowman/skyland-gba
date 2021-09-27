0ad8f08ab14453bf67166562a4d9801f36287d31
evanbowman <ebowman@bu.edu>
2021-09-27 08:42:33 -0400

    commit missing files

---
+++ source/data/skyland_title_3_flattened.h
@@ -0,0 +1,29 @@
+
+//{{BLOCK(skyland_title_3_flattened)
+
+//======================================================================
+//
+//	skyland_title_3_flattened, 3368x8@4, 
+//	Transparent color : FF,00,FF
+//	+ palette 256 entries, not compressed
+//	+ 421 tiles not compressed
+//	Total size: 512 + 13472 = 13984
+//
+//	Time-stamp: 2021-08-07, 17:06:57
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_SKYLAND_TITLE_3_FLATTENED_H
+#define GRIT_SKYLAND_TITLE_3_FLATTENED_H
+
+#define skyland_title_3_flattenedTilesLen 13472
+extern const unsigned int skyland_title_3_flattenedTiles[3368];
+
+#define skyland_title_3_flattenedPalLen 512
+extern const unsigned short skyland_title_3_flattenedPal[256];
+
+#endif // GRIT_SKYLAND_TITLE_3_FLATTENED_H
+
+//}}BLOCK(skyland_title_3_flattened)
