772288bcc6a8d227b2639fba43e0fe1eca007803
evanbowman <ebowman@bu.edu>
2021-09-27 08:43:09 -0400

    commit more missing files

---
+++ source/data/overlay_gamespeed.h
@@ -0,0 +1,29 @@
+
+//{{BLOCK(overlay_gamespeed)
+
+//======================================================================
+//
+//	overlay_gamespeed, 4032x8@4, 
+//	Transparent color : FF,00,FF
+//	+ palette 256 entries, not compressed
+//	+ 504 tiles not compressed
+//	Total size: 512 + 16128 = 16640
+//
+//	Time-stamp: 2021-08-07, 17:06:57
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_OVERLAY_GAMESPEED_H
+#define GRIT_OVERLAY_GAMESPEED_H
+
+#define overlay_gamespeedTilesLen 16128
+extern const unsigned int overlay_gamespeedTiles[4032];
+
+#define overlay_gamespeedPalLen 512
+extern const unsigned short overlay_gamespeedPal[256];
+
+#endif // GRIT_OVERLAY_GAMESPEED_H
+
+//}}BLOCK(overlay_gamespeed)
