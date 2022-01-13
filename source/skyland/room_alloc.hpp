974fcbcc560138a4a51111c3aa30665986716dbb
evanbowman <ebowman@bu.edu>
2022-01-12 19:40:07 -0500

    commit missing file

---
+++ source/skyland/room_alloc.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+
+#include "globals.hpp"
+
+
+
+namespace skyland {
+namespace room_pool {
+
+
+
+inline void deleter(Room* room)
+{
+    if (room) {
+        room->~Room();
+        auto& pools = std::get<SkylandGlobalData>(globals()).room_pools_;
+        pools.post(reinterpret_cast<byte*>(room));
+    }
+}
+
+
+
+template <typename T, typename... Args>
+RoomPtr<T> alloc(Args&&... args)
+{
+    auto& pool = std::get<SkylandGlobalData>(globals()).room_pools_;
+
+    static_assert(sizeof(T) <= max_room_size);
+    static_assert(alignof(T) <= alignment);
+
+    if (auto mem = pool.get()) {
+        new (mem) T(std::forward<Args>(args)...);
+
+        return RoomPtr<T>(reinterpret_cast<T*>(mem), deleter);
+    }
+
+    return {nullptr, deleter};
+}
+
+
+
+
+}
+}
