b63fea5b7a0463a7a1440a8896de1517b4e6da90
evanbowman <ebowman@bu.edu>
2022-01-28 08:25:10 -0500

    refactor flag designer, split off paint program

--- source/skyland/scene/modules/flagDesignerModule.cpp
+++ source/skyland/scene/modules/flagDesignerModule.cpp
@@ -12,8 +12,6 @@ namespace skyland {
 
 
 
-static const int canvas_start_x = 3;
-static const int canvas_start_y = 3;
 static const int view_shift = -18;
 
 
@@ -22,27 +20,14 @@ u32 flood_fill(Platform& pfrm, u8 matrix[16][16], u8 replace, u8 x, u8 y);
 
 
 
-void FlagDesignerModule::enter(Platform& pfrm, App& app, Scene& prev)
+void Paint::init(Platform& pfrm, App& app)
 {
-    pfrm.fill_overlay(0);
-    pfrm.screen().fade(1.f);
-
-    app.player_island().show_flag(true);
-
-    for (int x = 0; x < 13; ++x) {
-        for (int y = 0; y < 11; ++y) {
-            pfrm.set_tile(
-                Layer::overlay, x + canvas_start_x, y + canvas_start_y, 492);
-            pfrm.set_palette(
-                Layer::overlay, x + canvas_start_x, y + canvas_start_y, 0);
+    for (int x = 0; x < width_; ++x) {
+        for (int y = 0; y < height_; ++y) {
+            pfrm.set_tile(Layer::overlay, x + origin_x_, y + origin_y_, 492);
+            pfrm.set_palette(Layer::overlay, x + origin_x_, y + origin_y_, 0);
         }
     }
-
-    pfrm.load_tile0_texture("tilesheet");
-
-    View v;
-    v.set_center({0, view_shift});
-    pfrm.screen().set_view(v);
 
     // We use tiles to draw the large flag visualization, we need to re-order
     // the tiles based on the order of the player island's palette table.
@@ -58,6 +43,26 @@ void FlagDesignerModule::enter(Platform& pfrm, App& app, Scene& prev)
             pfrm.set_palette(Layer::overlay, td.data_[0][0] + 1, y, 0);
         }
     }
+}
+
+
+
+void FlagDesignerModule::enter(Platform& pfrm, App& app, Scene& prev)
+{
+    pfrm.fill_overlay(0);
+    pfrm.screen().fade(1.f);
+
+    app.player_island().show_flag(true);
+
+
+    pfrm.load_tile0_texture("tilesheet");
+
+    View v;
+    v.set_center({0, view_shift});
+    pfrm.screen().set_view(v);
+
+    Paint::init(pfrm, app);
+
 
     app.player_island().init_terrain(pfrm, 4);
     configure_island_from_codestring(
@@ -84,13 +89,32 @@ void FlagDesignerModule::exit(Platform& pfrm, App&, Scene& next)
 
 
 
-void FlagDesignerModule::draw_rulers(Platform& pfrm)
+void Paint::draw_rulers(Platform& pfrm)
 {
-    for (int i = 0; i < 13; ++i) {
+    for (int i = 0; i < width_; ++i) {
         pfrm.set_tile(Layer::overlay, 3 + i, 2, i == cursor_.x ? 474 : 472);
     }
-    for (int i = 0; i < 11; ++i) {
+    for (int i = 0; i < height_; ++i) {
         pfrm.set_tile(Layer::overlay, 16, 3 + i, i == cursor_.y ? 473 : 471);
+    }
+}
+
+
+
+void Paint::show(Platform& pfrm, App& app)
+{
+    for (int y = 0; y < height_; ++y) {
+        for (int x = 0; x < width_; ++x) {
+            const auto val = get_pixel(app, x, y);
+            if (val >= 16) {
+                Platform::fatal("pixel is not 4bit indexed color!");
+            }
+            const auto t = palette_[val];
+            pfrm.set_tile(
+                Layer::overlay, x + origin_x_, y + origin_y_, t);
+            pfrm.set_palette(
+                Layer::overlay, x + origin_x_, y + origin_y_, 0);
+        }
     }
 }
 
@@ -98,15 +122,7 @@ void FlagDesignerModule::draw_rulers(Platform& pfrm)
 
 void FlagDesignerModule::show(Platform& pfrm, App& app)
 {
-    for (int y = 0; y < 11; ++y) {
-        for (int x = 0; x < 13; ++x) {
-            const auto t = palette_[app.gp_.flag_img_.pixels[x][y]];
-            pfrm.set_tile(
-                Layer::overlay, x + canvas_start_x, y + canvas_start_y, t);
-            pfrm.set_palette(
-                Layer::overlay, x + canvas_start_x, y + canvas_start_y, 0);
-        }
-    }
+    Paint::show(pfrm, app);
 
     vram_write_flag(pfrm, app.gp_.flag_img_);
 }
@@ -115,6 +131,19 @@ void FlagDesignerModule::show(Platform& pfrm, App& app)
 
 ScenePtr<Scene>
 FlagDesignerModule::update(Platform& pfrm, App& app, Microseconds delta)
+{
+    if (app.player().key_down(pfrm, Key::action_2)) {
+        save::store_global_data(pfrm, app.gp_);
+        return scene_pool::alloc<TitleScreenScene>(3);
+    }
+
+    return Paint::update(pfrm, app, delta);
+}
+
+
+
+ScenePtr<Scene>
+Paint::update(Platform& pfrm, App& app, Microseconds delta)
 {
     if (app.player().key_down(pfrm, Key::alt_1)) {
         color_--;
@@ -136,7 +165,7 @@ FlagDesignerModule::update(Platform& pfrm, App& app, Microseconds delta)
         }
     }
     if (cursor_move_ready) {
-        if (app.player().key_pressed(pfrm, Key::right) and cursor_.x < 12) {
+        if (app.player().key_pressed(pfrm, Key::right) and cursor_.x < (width_ - 1)) {
             ++cursor_.x;
             ready_ = true;
             cursor_move_tic_ = milliseconds(90);
@@ -154,14 +183,14 @@ FlagDesignerModule::update(Platform& pfrm, App& app, Microseconds delta)
             cursor_move_tic_ = milliseconds(90);
             draw_rulers(pfrm);
         }
-        if (app.player().key_pressed(pfrm, Key::down) and cursor_.y < 10) {
+        if (app.player().key_pressed(pfrm, Key::down) and cursor_.y < (height_ - 1)) {
             ++cursor_.y;
             ready_ = true;
             cursor_move_tic_ = milliseconds(90);
             draw_rulers(pfrm);
         }
     } else {
-        if (app.player().key_down(pfrm, Key::right) and cursor_.x < 12) {
+        if (app.player().key_down(pfrm, Key::right) and cursor_.x < (width_ - 1)) {
             ++cursor_.x;
             ready_ = true;
             cursor_move_tic_ = milliseconds(400);
@@ -179,7 +208,7 @@ FlagDesignerModule::update(Platform& pfrm, App& app, Microseconds delta)
             cursor_move_tic_ = milliseconds(400);
             draw_rulers(pfrm);
         }
-        if (app.player().key_down(pfrm, Key::down) and cursor_.y < 10) {
+        if (app.player().key_down(pfrm, Key::down) and cursor_.y < (height_ - 1)) {
             ++cursor_.y;
             ready_ = true;
             cursor_move_tic_ = milliseconds(400);
@@ -190,22 +219,19 @@ FlagDesignerModule::update(Platform& pfrm, App& app, Microseconds delta)
     if (app.player().key_down(pfrm, Key::action_1)) {
         ready_ = true;
     }
-    if (app.player().key_down(pfrm, Key::action_2)) {
-        save::store_global_data(pfrm, app.gp_);
-        return scene_pool::alloc<TitleScreenScene>(3);
-    }
+
     if (app.player().key_down(pfrm, Key::start)) {
         ready_ = true;
         u8 temp[16][16];
-        for (int x = 0; x < FlagPixels::width; ++x) {
-            for (int y = 0; y < FlagPixels::height; ++y) {
-                temp[x][y] = app.gp_.flag_img_.pixels[x][y];
+        for (int x = 0; x < width(); ++x) {
+            for (int y = 0; y < height(); ++y) {
+                temp[x][y] = get_pixel(app, x, y);
             }
         }
         flood_fill(pfrm, temp, color_, cursor_.x, cursor_.y);
-        for (int x = 0; x < FlagPixels::width; ++x) {
-            for (int y = 0; y < FlagPixels::height; ++y) {
-                app.gp_.flag_img_.pixels[x][y] = temp[x][y];
+        for (int x = 0; x < width(); ++x) {
+            for (int y = 0; y < height(); ++y) {
+                set_pixel(app, x, y, temp[x][y]);
             }
         }
         show(pfrm, app);
@@ -215,7 +241,7 @@ FlagDesignerModule::update(Platform& pfrm, App& app, Microseconds delta)
         show(pfrm, app);
     }
     if (app.player().key_pressed(pfrm, Key::action_1) and ready_) {
-        app.gp_.flag_img_.pixels[cursor_.x][cursor_.y] = color_;
+        set_pixel(app, cursor_.x, cursor_.y, color_);
         show(pfrm, app);
     }
 
@@ -235,6 +261,13 @@ FlagDesignerModule::update(Platform& pfrm, App& app, Microseconds delta)
 
 void FlagDesignerModule::display(Platform& pfrm, App& app)
 {
+    return Paint::display(pfrm, app);
+}
+
+
+
+void Paint::display(Platform& pfrm, App& app)
+{
     app.player_island().display(pfrm);
 
     for (auto& effect : app.effects()) {
@@ -245,8 +278,8 @@ void FlagDesignerModule::display(Platform& pfrm, App& app)
     Sprite sprite;
     sprite.set_size(Sprite::Size::w16_h32);
     sprite.set_position({
-        -8 + cursor_.x * 8.f + 8 * canvas_start_x,
-        2 + cursor_.y * 8.f + 8 * canvas_start_y + view_shift,
+        -8 + cursor_.x * 8.f + 8 * origin_x_,
+        2 + cursor_.y * 8.f + 8 * origin_y_ + view_shift,
     });
 
     sprite.set_texture_index(62);
@@ -257,6 +290,26 @@ void FlagDesignerModule::display(Platform& pfrm, App& app)
     sprite.set_texture_index(63);
     sprite.set_position({4 + color_ * 8.f, 128 + view_shift});
     pfrm.screen().draw(sprite);
+}
+
+
+
+u8 FlagDesignerModule::get_pixel(App& app, u8 x, u8 y)
+{
+    if (x >= width() or y >= height()) {
+        return 111;
+    }
+    return app.gp_.flag_img_.pixels[x][y];
+}
+
+
+
+void FlagDesignerModule::set_pixel(App& app, u8 x, u8 y, u8 value)
+{
+    if (x >= width() or y >= height()) {
+        return;
+    }
+    app.gp_.flag_img_.pixels[x][y] = value;
 }
 
 
--- source/skyland/scene/modules/flagDesignerModule.hpp
+++ source/skyland/scene/modules/flagDesignerModule.hpp
@@ -10,8 +10,81 @@ namespace skyland {
 
 
 
-class FlagDesignerModule : public Module<FlagDesignerModule> {
+class Paint {
 public:
+
+    Paint(u8 width = 13,
+          u8 height = 11,
+          u8 origin_x = 3,
+          u8 origin_y = 3) :
+        width_(width),
+        height_(height),
+        origin_x_(origin_x),
+        origin_y_(origin_y)
+    {
+    }
+
+
+    virtual ~Paint()
+    {
+    }
+
+
+    ScenePtr<Scene> update(Platform& pfrm, App& app, Microseconds delta);
+
+
+    void display(Platform& pfrm, App& app);
+
+
+    void init(Platform&, App&);
+
+
+    virtual void show(Platform&, App&);
+    void draw_rulers(Platform&);
+
+
+    virtual u8 get_pixel(App& app, u8 x, u8 y) = 0;
+    virtual void set_pixel(App& app, u8 x, u8 y, u8 value) = 0;
+
+
+    u8 width() const
+    {
+        return width_;
+    }
+
+
+    u8 height() const
+    {
+        return height_;
+    }
+
+
+private:
+    Microseconds cursor_move_tic_ = 0;
+
+    bool ready_ = false;
+
+    u16 palette_[16];
+
+
+    Vec2<u8> cursor_;
+    u32 color_ = 0;
+
+    u8 width_;
+    u8 height_;
+    u8 origin_x_;
+    u8 origin_y_;
+
+};
+
+
+
+class FlagDesignerModule :
+        public Module<FlagDesignerModule>,
+        public Paint {
+
+public:
+
     static const char* module_name()
     {
         return "Flag Designer";
@@ -40,22 +113,14 @@ public:
     }
 
 
+    u8 get_pixel(App& app, u8 x, u8 y) override;
+    void set_pixel(App& app, u8 x, u8 y, u8 value) override;
+
+
+    void show(Platform&, App&) override;
+
+
 private:
-    Microseconds cursor_move_tic_ = 0;
-
-    bool ready_ = false;
-
-    u16 palette_[16];
-
-
-    void show(Platform&, App&);
-    void draw_rulers(Platform&);
-
-
-    Vec2<u8> cursor_;
-    u32 color_ = 0;
-
-
     static Factory factory_;
 };
 
