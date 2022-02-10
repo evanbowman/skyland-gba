15dff321dd5890215f54b8f41b745f6b86c7c201
evanbowman <ebowman@bu.edu>
2022-02-09 22:04:55 -0500

    refactor player classes

---
+++ source/skyland/opponent/friendlyAI.cpp
@@ -0,0 +1,22 @@
+#include "friendlyAI.hpp"
+#include "skyland/scene/scriptHookScene.hpp"
+#include "skyland/skyland.hpp"
+#include "enemyAI.hpp"
+
+
+
+namespace skyland {
+
+
+
+void FriendlyAI::on_room_damaged(Platform& pfrm, App& app, Room&)
+{
+    // What!? The player attacked us! We're no longer a friendly AI.
+    app.swap_opponent<EnemyAI>();
+
+    invoke_hook(pfrm, app, "on-hostile-transition");
+}
+
+
+
+}
---
+++ source/skyland/player/player.hpp
@@ -0,0 +1,132 @@
+#pragma once
+
+
+#include "number/numeric.hpp"
+#include "platform/key.hpp"
+
+
+
+class Platform;
+
+
+
+namespace skyland {
+
+
+
+class Island;
+class Room;
+class App;
+
+
+
+using MissileAmmo = short int;
+
+
+
+class Player {
+public:
+    virtual ~Player()
+    {
+    }
+
+
+    virtual void update(Platform&, App&, Microseconds delta)
+    {
+    }
+
+
+    virtual void on_layout_changed(Platform& pfrm, App& app, Island& island)
+    {
+    }
+
+
+    virtual void on_room_damaged(Platform& pfrm, App& app, Room& room)
+    {
+    }
+
+
+    virtual void on_room_destroyed(Platform& pfrm, App& app, Room& room)
+    {
+    }
+
+
+    virtual void on_room_plundered(Platform& pfrm, App& app, Room& room)
+    {
+    }
+
+
+    virtual bool key_up(Platform&, Key k)
+    {
+        return false;
+    }
+
+
+    virtual bool key_down(Platform&, Key k)
+    {
+        return false;
+    }
+
+
+    virtual bool key_pressed(Platform&, Key k)
+    {
+        return false;
+    }
+
+
+    // key_held and key_held_reset should implement some sort of timer-based key
+    // states.
+    virtual bool key_held(Key k, Microseconds duration)
+    {
+        return false;
+    }
+
+
+    virtual void key_held_reset(Key k, Microseconds decrement)
+    {
+    }
+
+
+    // Should be implemented to treat all currently pressed keys as held for the
+    // same amount of time as the longest held key. Used to implement smooth
+    // scroll locking, where we want the scroll lock to continue in a different
+    // direction if the player changes direction with the d-pad. E.g. press and
+    // hold to scroll continuously (see test_key()), then change direction, the
+    // scrolling would halt until the new direction builds up enough inertia to
+    // start scrolling continuously, unless we grant the new direction the same
+    // scrolling inertia as the previous direction button.
+    virtual void key_held_distribute(Platform& pfrm)
+    {
+    }
+
+
+    bool test_key(Platform& pfrm,
+                  Key k,
+                  Microseconds held_time,
+                  Microseconds held_decrement)
+    {
+        if (key_down(pfrm, k) or key_held(k, held_time)) {
+            key_held_reset(k, held_decrement);
+            return true;
+        }
+        return false;
+    }
+
+
+    int rooms_lost_ = 0;
+    int rooms_built_ = 0;
+
+
+    MissileAmmo& missile_ammo()
+    {
+        return missile_ammo_;
+    }
+
+
+private:
+    MissileAmmo missile_ammo_;
+};
+
+
+
+} // namespace skyland
