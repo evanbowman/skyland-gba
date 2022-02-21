c189902d866593faf31dee8b2c24527a310bfb0e
evanbowman <ebowman@bu.edu>
2022-02-21 11:51:04 -0500

    edit coin yield for easy mode in SKYLAND Forever

--- scripts/config/forever/easy.lisp
+++ scripts/config/forever/easy.lisp
@@ -5,9 +5,9 @@
 (coins-add 4000)
 
 (map (lambda (syscall "setvar" (cdr $0) (car $0)))
- '((38 . "sf_p1_coin_yield")
-   (28 . "sf_p2_coin_yield")
-   (20 . "sf_p3_coin_yield")
-   (12 . "sf_p4_coin_yield")))
+ '((40 . "sf_p1_coin_yield")
+   (32 . "sf_p2_coin_yield")
+   (26 . "sf_p3_coin_yield")
+   (18 . "sf_p4_coin_yield")))
 
 (syscall "setvar" "score_multiplier" 1)
