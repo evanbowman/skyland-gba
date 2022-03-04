49e913b9ff62656d5d40a7450d9feb00d15915bf
evanbowman <ebowman@bu.edu>
2022-03-03 22:05:00 -0500

    implement delivery level

--- scripts/event/quest_marker/delivery.lisp
+++ scripts/event/quest_marker/delivery.lisp
@@ -1,9 +1,7 @@
-;;;
-;;;
-;;;
+
+(dialog "You arrive at the delivery address...")
 
 
-; TODO...
 
 (opponent-init 6 'neutral)
 
@@ -12,4 +10,33 @@
  '((power-core 3 13)
    (coconut-palm 5 13)))
 
-(exit)
+
+(setq on-converge
+      (lambda
+        (let ((c (cargo-bays (player))))
+          (let ((p (filter
+                    (lambda
+                      (equal
+                       "parcel"
+                       (cargo (player) (car $0) (cdr $0))))
+                    c)))
+            (if p
+                (let ((temp (+ 1500 (choice 2000))))
+                  ;; Clear out cargo
+                  (cargo-set
+                   (player)
+                   (car (car p))
+                   (cdr (car p))
+                   "")
+
+                  (dialog
+                   "<c:Margot:6>Thank you! I wasn't sure it would arrive! Here's "
+                   (string temp)
+                   "@ for your trouble!")
+
+                  (coins-add temp)
+
+                  (setq on-dialog-closed exit))
+              (progn
+                (setq on-dialog-closed exit)
+                (dialog "Upon closer inspection, no one seems to be home! Good thing too, as it seems that you lost the parcel along the way!")))))))
