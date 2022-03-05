be4fd7b89c1e979bbda07fe7acd8c946c9b6e2d6
evanbowman <ebowman@bu.edu>
2022-03-05 10:07:58 -0500

    add sunflowers to title page

--- source/data/skyland_title_3_flattened.h
+++ source/data/skyland_title_3_flattened.h
@@ -9,7 +9,7 @@
 //	+ 481 tiles not compressed
 //	Total size: 512 + 15392 = 15904
 //
-//	Time-stamp: 2022-02-17, 18:20:57
+//	Time-stamp: 2022-03-05, 09:56:36
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
 //	( http://www.coranac.com/projects/#grit )
 //
