3b1f38b1c0585394eedbb4c7053c6dd6d21fb5c8
evanbowman <ebowman@bu.edu>
2022-03-24 21:33:53 -0400

    commit missing header

---
+++ source/skyland/stateBit.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "bitvector.hpp"
+
+
+
+namespace skyland
+{
+
+
+
+enum class StateBit {
+    dialog_expects_answer,
+    launch_repl,
+    surrender_offered,
+    count,
+};
+
+
+
+class App;
+
+
+
+void state_bit_store(App& app, StateBit state_bit, bool value);
+
+
+
+bool state_bit_load(App& app, StateBit state_bit);
+
+
+
+using StateBitvector = Bitvector<(int)StateBit::count>;
+
+
+
+} // namespace skyland
