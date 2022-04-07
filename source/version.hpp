1618ebdc438567709355d2ed7f91dd5a27cb08cf
evanbowman <ebowman@bu.edu>
2022-04-07 19:37:40 -0400

    "bump version"

--- source/version.hpp
+++ source/version.hpp
@@ -1,27 +1,5 @@
-////////////////////////////////////////////////////////////////////////////////
-//
-// Copyright (C) 2022  Evan Bowman
-//
-// This program is free software; you can redistribute it and/or modify it under
-// the terms of version 2 of the GNU General Public License as published by the
-// Free Software Foundation.
-//
-// This program is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-// details.
-//
-// You should have received a copy of the GNU General Public License along with
-// this program; if not, write to the Free Software Foundation, Inc., 51
-// Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-//
-// GPL2 ONLY. No later versions permitted.
-//
-////////////////////////////////////////////////////////////////////////////////
-
-
 #pragma once
 #define PROGRAM_MAJOR_VERSION 2022
 #define PROGRAM_MINOR_VERSION 4
-#define PROGRAM_SUBMINOR_VERSION 4
-#define PROGRAM_VERSION_REVISION 2
+#define PROGRAM_SUBMINOR_VERSION 7
+#define PROGRAM_VERSION_REVISION 0
