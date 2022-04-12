4bcdb8ec64297eebd89ba5539d3cc02b4f147dd6
evanbowman <ebowman@bu.edu>
2022-04-12 09:50:45 -0400

    commit missing header for gold block

---
+++ source/skyland/rooms/gold.hpp
@@ -0,0 +1,103 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2022  Evan Bowman
+//
+// This program is free software; you can redistribute it and/or modify it under
+// the terms of version 2 of the GNU General Public License as published by the
+// Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program; if not, write to the Free Software Foundation, Inc., 51
+// Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// GPL2 ONLY. No later versions permitted.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+#pragma once
+
+
+#include "decoration.hpp"
+#include "skyland/systemString.hpp"
+#include "skyland/tile.hpp"
+
+
+
+namespace skyland
+{
+
+
+
+class Gold : public Decoration
+{
+public:
+
+    Gold(Island* parent, const RoomCoord& position)
+        : Decoration(parent, name(), position)
+    {
+    }
+
+
+    static void format_description(Platform& pfrm, StringBuffer<512>& buffer)
+    {
+        buffer += SYSTR(description_gold)->c_str();
+    }
+
+
+    void render_interior(App& app, TileId buffer[16][16]) override
+    {
+        buffer[position().x][position().y] = InteriorTile::gold;
+    }
+
+
+    void render_exterior(App& app, TileId buffer[16][16]) override
+    {
+        buffer[position().x][position().y] = Tile::gold;
+    }
+
+
+    static RoomProperties::Bitmask properties()
+    {
+        return Decoration::properties();
+    }
+
+
+    static const char* name()
+    {
+        return "gold";
+    }
+
+
+    static SystemString ui_name()
+    {
+        return SystemString::block_gold;
+    }
+
+
+    static Vec2<u8> size()
+    {
+        return {1, 1};
+    }
+
+
+    static Icon icon()
+    {
+        return 2440;
+    }
+
+
+    static Icon unsel_icon()
+    {
+        return 2456;
+    }
+};
+
+
+
+} // namespace skyland
