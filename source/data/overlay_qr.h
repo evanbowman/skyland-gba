2a532678428eb39d103584440dd3d282ec62e15b
evanbowman <ebowman@bu.edu>
2022-04-19 09:02:23 -0400

    commit stuff

---
+++ source/data/overlay_qr.h
@@ -0,0 +1,29 @@
+
+//{{BLOCK(overlay_qr)
+
+//======================================================================
+//
+//	overlay_qr, 664x8@4, 
+//	Transparent color : FF,00,FF
+//	+ palette 256 entries, not compressed
+//	+ 83 tiles not compressed
+//	Total size: 512 + 2656 = 3168
+//
+//	Time-stamp: 2022-04-14, 21:17:10
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_OVERLAY_QR_H
+#define GRIT_OVERLAY_QR_H
+
+#define overlay_qrTilesLen 2656
+extern const unsigned int overlay_qrTiles[664];
+
+#define overlay_qrPalLen 512
+extern const unsigned short overlay_qrPal[256];
+
+#endif // GRIT_OVERLAY_QR_H
+
+//}}BLOCK(overlay_qr)
