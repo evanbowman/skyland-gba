293c4e043582c6c9267d170b863ac3a60cf7f683
evanbowman <ebowman@bu.edu>
2022-04-26 23:14:23 -0400

    replace unused color in palette...
    with a gross pink color, as a reminder that it's available if needed

--- source/data/macro_rendertexture.h
+++ source/data/macro_rendertexture.h
@@ -9,7 +9,7 @@
 //	+ 750 tiles not compressed
 //	Total size: 512 + 24000 = 24512
 //
-//	Time-stamp: 2022-04-25, 09:44:43
+//	Time-stamp: 2022-04-26, 23:14:19
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
 //	( http://www.coranac.com/projects/#grit )
 //
