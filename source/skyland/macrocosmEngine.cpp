ea4f63cb8a85f1f65b59de776ea834fcffcfc830
evanbowman <ebowman@bu.edu>
2022-04-26 22:29:06 -0400

    bugfix (sunflowers should block light)

--- source/skyland/macrocosmEngine.cpp
+++ source/skyland/macrocosmEngine.cpp
@@ -1296,7 +1296,7 @@ static bool blocks_light(terrain::Type t)
     static const bool result[(int)terrain::Type::count] = {
         false, true, true, true, true,  true,  false, true,
         true,  true, true, true, false, false, false, false,
-        true,  true, true, true, true,  true,  false,
+        true,  true, true, true, true,  true,  true, false,
     };
 
     return result[(int)t];
