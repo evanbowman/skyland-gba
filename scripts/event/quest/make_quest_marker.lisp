1e02f342c5990003705f18d099e5718877ba577a
evanbowman <ebowman@bu.edu>
2022-06-23 22:45:58 -0400

    rename vars in quest marker script

--- scripts/event/quest/make_quest_marker.lisp
+++ scripts/event/quest/make_quest_marker.lisp
@@ -4,7 +4,7 @@
       (c (filter
           (lambda (equal (car $0) 4))
           (wg-nodes)))
-      (p (wg-pos)))
+      (pos (wg-pos)))
 
   ;; Sort corrupt nodes by x-value. We don't want to place a quest marker too
   ;; close to the storm frontier.
@@ -18,7 +18,7 @@
                (not (equal (car $0) 0))
                (not (equal (car $0) 4))
                (not (equal (car $0) 5))
-               (not (equal $0 p))
+               (not (equal $0 pos))
                ;; The node must be somewhat ahead of the storm frontier; we
                ;; can't just place a node somewhere that's totally unreachable,
                ;; that's no fun.
@@ -35,10 +35,10 @@
                    (lambda
                      ;; Sort by manhattan length from current coord,
                      ;; descending.
-                     (> (+ (abs (- (car (cdr $0)) (car (cdr p))))
-                           (abs (- (cdr (cdr $0)) (cdr (cdr p)))))
-                        (+ (abs (- (car (cdr $1)) (car (cdr p))))
-                           (abs (- (cdr (cdr $1)) (cdr (cdr p)))))))))
+                     (> (+ (abs (- (car (cdr $0)) (car (cdr pos))))
+                           (abs (- (cdr (cdr $0)) (cdr (cdr pos)))))
+                        (+ (abs (- (car (cdr $1)) (car (cdr pos))))
+                           (abs (- (cdr (cdr $1)) (cdr (cdr pos)))))))))
           (let ((n (get n (choice 3))))
             (wg-node-set
              (car (cdr n))
