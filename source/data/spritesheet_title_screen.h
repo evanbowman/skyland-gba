6f2b675446efe643e5f1fd1996d1ef05409cee99
evanbowman <ebowman@bu.edu>
2022-06-30 17:24:26 -0400

    spritesheet edits

--- source/data/spritesheet_title_screen.h
+++ source/data/spritesheet_title_screen.h
@@ -9,7 +9,7 @@
 //	+ 1744 tiles Metatiled by 2x4 not compressed
 //	Total size: 512 + 55808 = 56320
 //
-//	Time-stamp: 2022-06-30, 15:25:38
+//	Time-stamp: 2022-06-30, 16:18:29
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
 //	( http://www.coranac.com/projects/#grit )
 //
