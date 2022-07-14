18d5b0737f0090e77d2052795bf2a110e8857b8d
evanbowman <ebowman@bu.edu>
2022-07-14 17:01:30 -0400

    misc edits

--- source/data/macro_rendertexture.h
+++ source/data/macro_rendertexture.h
@@ -3,13 +3,13 @@
 
 //======================================================================
 //
-//	macro_rendertexture, 10256x8@4, 
+//	macro_rendertexture, 10512x8@4, 
 //	Transparent color : FF,00,FF
 //	+ palette 256 entries, not compressed
-//	+ 1282 tiles not compressed
-//	Total size: 512 + 41024 = 41536
+//	+ 1314 tiles not compressed
+//	Total size: 512 + 42048 = 42560
 //
-//	Time-stamp: 2022-07-14, 11:51:04
+//	Time-stamp: 2022-07-14, 17:01:21
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -18,8 +18,8 @@
 #ifndef GRIT_MACRO_RENDERTEXTURE_H
 #define GRIT_MACRO_RENDERTEXTURE_H
 
-#define macro_rendertextureTilesLen 41024
-extern const unsigned int macro_rendertextureTiles[10256];
+#define macro_rendertextureTilesLen 42048
+extern const unsigned int macro_rendertextureTiles[10512];
 
 #define macro_rendertexturePalLen 512
 extern const unsigned short macro_rendertexturePal[256];
