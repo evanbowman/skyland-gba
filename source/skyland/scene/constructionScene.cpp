625622748705a8912e839d4f0c0d339cc668d320
evanbowman <ebowman@bu.edu>
2022-08-15 13:46:10 -0400

    update ui for adding terrain

--- source/skyland/scene/constructionScene.cpp
+++ source/skyland/scene/constructionScene.cpp
@@ -921,6 +921,20 @@ void ConstructionScene::display(Platform& pfrm, App& app)
         sprite.set_size(Sprite::Size::w16_h32);
         sprite.set_position(origin);
         pfrm.screen().draw(sprite);
+        {
+            origin = island(app)->visual_origin();
+            origin.x += (island(app)->terrain().size() - 1) * 16;
+            origin.y += 15 * 16;
+
+            sprite.set_position(origin);
+            sprite.set_texture_index(99);
+            sprite.set_alpha(Sprite::Alpha::translucent);
+            pfrm.screen().draw(sprite);
+            sprite.set_texture_index(100);
+            origin.x += 16;
+            sprite.set_position(origin);
+            pfrm.screen().draw(sprite);
+        }
         break;
     }
     }
