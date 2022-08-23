bcd900499f0e466353b74dc694abc635dc3f09ae
evanbowman <ebowman@bu.edu>
2022-08-22 22:08:09 -0400

    add neutral level template for zone 1

--- scripts/event/neutral/0/4.lisp
+++ scripts/event/neutral/0/4.lisp
@@ -3,55 +3,45 @@
 ;;;
 
 
-(dialog "A distress signal sounds over your radio. You change course to investigate...")
+(dialog "You discover the wreckage of a goblin raid...")
 
 
-(opponent-init 5 'neutral)
-
+(opponent-init 8 'neutral)
 
 (island-configure
  (opponent)
- '((power-core 3 13)
-   (hull 0 14)))
+ '((hull 0 14)
+   (hull 0 13)
+   (hull 0 12)
+   (masonry 1 14)
+   (masonry 1 13)
+   (masonry 2 14)
+   (plundered-room 2 12)
+   (masonry 3 14)
+   (plundered-room 3 12)
+   (masonry 4 14)
+   (power-core 5 13)
+   (rocket-bomb 7 12)))
 
 
-(chr-new (opponent) 1 14 'neutral 0)
-(chr-new (opponent) 2 14 'neutral 0)
+(defn on-converge
+  (dialog
+   "The island seems thoroughly ransacked... but the pirates inexplicably "
+   "left behind a weapon. Haul it aboard?")
+  (dialog-await-y/n))
 
 
-(setq on-converge
-      (lambda
-        (dialog "You discover a damaged fortress. Spend 800@ in resources to rescue the survivors?")
-        (dialog-await-y/n)
-        (setq on-converge nil)))
+(defn on-dialog-accepted
+  (while (not (construction-sites (player) '(1 . 3)))
+    (terrain (player) (+ (terrain (player)) 1)))
+  (room-del (opponent) 7 12)
+  (sel-input '(1 . 3)
+             "Pick a slot (1, 3)"
+             (lambda
+               (room-new (player) (list 'rocket-bomb $1 $2))
+               (dialog "A great addition!")
+               (setq on-dialog-closed exit))))
 
-
-(setq on-dialog-accepted
-      (lambda
-        (setq temp (chr-slots (player)))
-
-        (if temp
-            (progn
-              (coins-add -800)
-              (setq temp (get temp (choice (length temp))))
-              (chr-new (player) (car temp) (cdr temp) 'neutral 0)
-              (chr-del (opponent) 1 14)
-
-              (setq temp (chr-slots (player)))
-              (if temp
-                  (progn
-                    (setq temp (get temp (choice (length temp))))
-                    (chr-new (player) (car temp) (cdr temp) 'neutral 0)
-                    (chr-del (opponent) 2 14)
-                    (dialog "Two survivors joined your crew!"))
-                (dialog "You rescue one of the survivors. The other survivor decides "
-                        "that your castle is too crowded and declines to come aboard.")))
-          (dialog "Sadly, there's no room in your castle for anyone else"))
-
-        (exit)))
-
-
-(setq on-dialog-declined
-      (lambda
-        ;; TODO...
-        (exit)))
+(defn on-dialog-declined
+  (dialog "Huh!? Who doesn't want free stuff? Suit yourself...")
+  (setq on-dialog-closed exit))
