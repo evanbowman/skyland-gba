c5235695a88d9cc736a64f17a2fa50ca8f05ec00
evanbowman <ebowman@bu.edu>
2023-05-23 19:08:08 -0400

    commit missing data files

---
+++ source/data/background_macro_night.h
@@ -0,0 +1,29 @@
+
+//{{BLOCK(background_macro_night)
+
+//======================================================================
+//
+//	background_macro_night, 1024x8@4, 
+//	Transparent color : FF,00,FF
+//	+ palette 16 entries, not compressed
+//	+ 128 tiles lz77 compressed
+//	Total size: 32 + 596 = 628
+//
+//	Time-stamp: 2023-03-25, 09:25:55
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_BACKGROUND_MACRO_NIGHT_H
+#define GRIT_BACKGROUND_MACRO_NIGHT_H
+
+#define background_macro_nightTilesLen 596
+extern const unsigned int background_macro_nightTiles[149];
+
+#define background_macro_nightPalLen 32
+extern const unsigned short background_macro_nightPal[16];
+
+#endif // GRIT_BACKGROUND_MACRO_NIGHT_H
+
+//}}BLOCK(background_macro_night)
