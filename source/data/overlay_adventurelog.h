69a79296a46e7df1391c8deafeec2bce1e9ced70
evanbowman <ebowman@bu.edu>
2023-06-23 11:27:35 -0400

    commit missing files

---
+++ source/data/overlay_adventurelog.h
@@ -0,0 +1,29 @@
+
+//{{BLOCK(overlay_adventurelog)
+
+//======================================================================
+//
+//	overlay_adventurelog, 1176x8@4, 
+//	Transparent color : FF,00,FF
+//	+ palette 16 entries, not compressed
+//	+ 147 tiles lz77 compressed
+//	Total size: 32 + 724 = 756
+//
+//	Time-stamp: 2023-06-22, 16:27:21
+//	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_OVERLAY_ADVENTURELOG_H
+#define GRIT_OVERLAY_ADVENTURELOG_H
+
+#define overlay_adventurelogTilesLen 724
+extern const unsigned int overlay_adventurelogTiles[181];
+
+#define overlay_adventurelogPalLen 32
+extern const unsigned short overlay_adventurelogPal[16];
+
+#endif // GRIT_OVERLAY_ADVENTURELOG_H
+
+//}}BLOCK(overlay_adventurelog)
