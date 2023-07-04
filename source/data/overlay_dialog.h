7ed0b943b72a0cc6b4af1581863a82d07eaebf3c
evanbowman <ebowman@bu.edu>
2023-07-04 16:09:59 -0400

    adjust dialog box images

--- source/data/overlay_dialog.h
+++ source/data/overlay_dialog.h
@@ -9,7 +9,7 @@
 //	+ 463 tiles not compressed
 //	Total size: 32 + 14816 = 14848
 //
-//	Time-stamp: 2023-07-01, 08:03:01
+//	Time-stamp: 2023-07-04, 15:47:41
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
