f81f682928192d4aa176bd41ac7c1c3d4b552473
evanbowman <ebowman@bu.edu>
2023-12-01 08:40:09 -0500

    replace license

--- source/skyland/rooms/clumpBomb.hpp
+++ source/skyland/rooms/clumpBomb.hpp
@@ -1,21 +1,33 @@
 ////////////////////////////////////////////////////////////////////////////////
 //
-// Copyright (C) 2023  Evan Bowman
+// Copyright (C) 2023  Evan Bowman. Some rights reserved.
 //
-// This program is free software; you can redistribute it and/or modify it under
-// the terms of version 2 of the GNU General Public License as published by the
-// Free Software Foundation.
+// This program is source-available; the source code is provided for educational
+// purposes. All copies of the software must be distributed along with this
+// license document.
 //
-// This program is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-// details.
+// 1. DEFINITION OF SOFTWARE: The term "Software" refers to the SKYLAND,
+// including any updates, modifications, or associated documentation provided by
+// Licensor.
 //
-// You should have received a copy of the GNU General Public License along with
-// this program; if not, write to the Free Software Foundation, Inc., 51
-// Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+// 2. DERIVATIVE WORKS: Licensee is permitted to modify the source code.
 //
-// GPL2 ONLY. No later versions permitted.
+// 3. COMMERCIAL USE: Commercial use is not allowed.
+//
+// 4. ATTRIBUTION: Licensee is required to provide attribution to Licensor.
+//
+// 5. INTELLECTUAL PROPERTY RIGHTS: All intellectual property rights in the
+// Software shall remain the property of Licensor. The Licensee does not acquire
+// any rights to the Software except for the limited use rights specified in
+// this Agreement.
+//
+// 6. WARRANTY AND LIABILITY: The Software is provided "as is" without warranty
+// of any kind. Licensor shall not be liable for any damages arising out of or
+// related to the use or inability to use the Software.
+//
+// 7. TERMINATION: This Agreement shall terminate automatically if Licensee
+// breaches any of its terms and conditions. Upon termination, Licensee must
+// cease all use of the Software and destroy all copies.
 //
 ////////////////////////////////////////////////////////////////////////////////
 
