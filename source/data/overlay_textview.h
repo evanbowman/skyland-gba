9404fdd1320f2821df51c3405d4a1a1ee44bcb7e
evanbowman <ebowman@bu.edu>
2024-01-10 10:52:48 -0500

    commit missing overlay

---
+++ source/data/overlay_textview.h
@@ -0,0 +1,29 @@
+
+//{{BLOCK(overlay_textview)
+
+//======================================================================
+//
+//	overlay_textview, 3000x8@4, 
+//	Transparent color : FF,00,FF
+//	+ palette 16 entries, not compressed
+//	+ 375 tiles lz77 compressed
+//	Total size: 32 + 1580 = 1612
+//
+//	Time-stamp: 2023-12-25, 09:11:28
+//	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_OVERLAY_TEXTVIEW_H
+#define GRIT_OVERLAY_TEXTVIEW_H
+
+#define overlay_textviewTilesLen 1580
+extern const unsigned int overlay_textviewTiles[395];
+
+#define overlay_textviewPalLen 32
+extern const unsigned short overlay_textviewPal[16];
+
+#endif // GRIT_OVERLAY_TEXTVIEW_H
+
+//}}BLOCK(overlay_textview)
