58f9426bbae3c62856f0a80f14beb9374dcaeb74
Evan Bowman <evan.a.bowman@gmail.com>
2024-09-27 19:52:01 -0400

    turning off permadeath disables highscores

--- source/skyland/scene/highscoresScene.cpp
+++ source/skyland/scene/highscoresScene.cpp
@@ -290,7 +290,9 @@ void HighscoresScene::enter(Scene& prev)
 
     if (not APP.is_developer_mode() and
         not(APP.persistent_data().state_flags_.get() &
-            PersistentData::StateFlag::dev_mode_active)) {
+            PersistentData::StateFlag::dev_mode_active) and
+        not(APP.persistent_data().state_flags_.get() &
+            PersistentData::StateFlag::permadeath_on)) {
 
         if (score > (int)highscores.values_[0].get()) {
             highscore_island_info_store();
