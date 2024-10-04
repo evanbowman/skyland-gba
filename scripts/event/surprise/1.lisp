7a4444dd57f69e935e8badd734b6e54cb0652f86
Evan Bowman <evan.a.bowman@gmail.com>
2024-10-04 17:31:48 -0400

    fixes to mimic ship level

--- scripts/event/surprise/1.lisp
+++ scripts/event/surprise/1.lisp
@@ -3,7 +3,7 @@
 ;;;
 
 
-(let ((bonus (get '(1500 2500 3500 4000) (zone))))
+(let ((bonus (get '(1500 2500 4500 6000) (zone))))
   (dialog "An eerie wind blows through the night... <B:0> "
           (format "An alarm beacon wakes your crew. You investigate, and discover %@! " bonus)
           "<B:0> But just then, a trap springs... and you find yourself facing a devious opponent... the dreaded mimic ship!")
@@ -17,6 +17,12 @@
 (opponent-init 7 'hostile)
 (run-util-script "clone-isle" (player) (opponent))
 
+(when (< (difficulty) 2)
+  (foreach (lambda (room)
+             ;; Don't make the player fight a ship with atomic warheads unless
+             ;; we're in hard mode.
+             (if (equal (car room) 'warhead)
+                 (room-mut (opponent) (get room 1) (get room 2) 'missile-silo)))
+           (rooms (opponent))))
 
-(push-hook 'on-victory (lambda ()
-                         (achieve 13)))
+(achieve 13)
