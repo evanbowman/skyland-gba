621d1bf6b6dc3732b01c4bb07341524503212c21
Evan Bowman <evan.a.bowman@gmail.com>
2024-10-05 12:18:00 -0400

    increase cost of mirror-hull

--- scripts/config/room_tab.lisp
+++ scripts/config/room_tab.lisp
@@ -10,7 +10,7 @@
 '((hull         240 300  0)
   (code         240 300  0)
   (bronze-hull  240 300  0)
-  (mirror-hull  240 500  0)
+  (mirror-hull  240 700  0)
   (stacked-hull 200 600  0)
   (barrier      1   300  0)
   (forcefield   240 300  40)
