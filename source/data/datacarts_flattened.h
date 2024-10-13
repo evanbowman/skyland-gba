443ded5ad9286743522d3e31c2bb9da90e4fff3f
Evan Bowman <evan.a.bowman@gmail.com>
2024-10-13 15:31:22 -0400

    update images, strings

--- source/data/datacarts_flattened.h
+++ source/data/datacarts_flattened.h
@@ -7,9 +7,9 @@
 //	Transparent color : FF,00,FF
 //	+ palette 16 entries, not compressed
 //	+ 421 tiles lz77 compressed
-//	Total size: 32 + 2744 = 2776
+//	Total size: 32 + 2760 = 2792
 //
-//	Time-stamp: 2024-10-13, 14:06:37
+//	Time-stamp: 2024-10-13, 15:31:06
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -18,8 +18,8 @@
 #ifndef GRIT_DATACARTS_FLATTENED_H
 #define GRIT_DATACARTS_FLATTENED_H
 
-#define datacarts_flattenedTilesLen 2744
-extern const unsigned int datacarts_flattenedTiles[686];
+#define datacarts_flattenedTilesLen 2760
+extern const unsigned int datacarts_flattenedTiles[690];
 
 #define datacarts_flattenedPalLen 32
 extern const unsigned short datacarts_flattenedPal[16];
