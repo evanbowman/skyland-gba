979c87e56e0efd2b05f9b6988c513b2ae1d0d3a3
Evan Bowman <evan.a.bowman@gmail.com>
2024-11-02 18:10:46 -0400

    commit spritesheet changes

--- source/data/spritesheet.h
+++ source/data/spritesheet.h
@@ -9,7 +9,7 @@
 //	+ 1744 tiles Metatiled by 2x4 not compressed
 //	Total size: 32 + 55808 = 55840
 //
-//	Time-stamp: 2024-11-02, 17:39:27
+//	Time-stamp: 2024-11-02, 18:09:50
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
