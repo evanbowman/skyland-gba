00eed9f2fa33c1cc0c0f5d21013a00a49a4f5c75
Evan Bowman <evan.a.bowman@gmail.com>
2024-11-19 17:41:59 -0500

    fix island placement in import-level function

--- source/skyland/script_defs.cpp
+++ source/skyland/script_defs.cpp
@@ -1744,6 +1744,10 @@ BINDING_TABLE({
           L_EXPECT_OP(1, integer);
           L_EXPECT_OP(0, integer);
           auto island = unwrap_isle(lisp::get_op(2));
+          if (island == APP.opponent_island()) {
+              island->set_drift(Fixnum(-0.000025f));
+          }
+
           island->set_position({Fixnum::from_integer(L_LOAD_INT(1)),
                                 Fixnum::from_integer(L_LOAD_INT(0))});
           return L_NIL;
