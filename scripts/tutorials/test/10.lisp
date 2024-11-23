9e24a7ae8a5e67bafc114e4c424124f513cf98a4
Evan Bowman <evan.a.bowman@gmail.com>
2024-11-23 07:19:55 -0500

    regression tests for replicators

--- scripts/tutorials/test/10.lisp
+++ scripts/tutorials/test/10.lisp
@@ -1,3 +1,12 @@
 ;;;
 ;;; replicator.lisp
 ;;;
+
+
+(assert-eq (chrs (player))
+           '((4 14 (hp . 191) (rplc . 1) (sc . 12) (id . 3))
+             (3 14 (hp . 143) (rplc . 1) (id . 5))
+             (2 14 (hp . 191) (rplc . 1) (sc . 9) (id . 4))
+             (1 14 (sc . 18) (id . 1))))
+
+(assert-eq (coins) 17900)
