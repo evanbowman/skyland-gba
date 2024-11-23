ed77929db690645af28a1ab23376e4c8165173b8
Ophir Doron <tulidoron@gmail.com>
2024-11-23 18:41:58 +0200

    Update tutorials.lisp (#98)
    * Update tutorials.lisp
    
    show template in dev mode
    
    * Update tutorials.lisp
    
    * Update 15.lisp
    
    * Create 16.lisp

--- scripts/tutorials/test/15.lisp
+++ scripts/tutorials/test/15.lisp
@@ -1,3 +1,7 @@
+;;;
+;;; portals.lisp
+;;;
+
 (assert-eq (rooms (player))
            '((infirmary 0 13) (portal 2 14) (power-core 3 13) (workshop 3 11) (portal 3 10) (reactor 4 8) (portal 5 12) (manufactory 5 13) (hull 6 12) (missile-silo 6 10) (portal 7 12) (hull 7 11) (reactor 7 8) (stairwell 8 11)))
 
