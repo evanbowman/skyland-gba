4c6dd165fe2b72cb337a74bb390df09752673f08
Evan Bowman <evan.a.bowman@gmail.com>
2024-11-28 13:36:00 -0500

    amplifier emits a ripple effect

--- source/skyland/rooms/amplifier.hpp
+++ source/skyland/rooms/amplifier.hpp
@@ -45,6 +45,55 @@ namespace skyland
 
 
 
+class AmpAnim : public Entity
+{
+public:
+    AmpAnim(Vec2<Fixnum> pos) : Entity({})
+    {
+        sprite_.set_size(Sprite::Size::w16_h16);
+        sprite_.set_tidx_16x16(31, 0);
+        sprite_.set_position(pos);
+        sprite_.set_origin({});
+        sprite_.set_mix({ColorConstant::electric_blue, 255});
+    }
+
+
+    void update(Time delta) override
+    {
+        delta = PLATFORM.delta_clock().last_delta();
+
+        timer_ += delta;
+        if (timer_ >= milliseconds(80)) {
+            timer_ -= milliseconds(80);
+            auto t = sprite_.get_texture_index();
+            if (t == 31 * 2 + 5) {
+                kill();
+                return;
+            }
+
+            ++t;
+            sprite_.set_texture_index(t);
+        }
+    }
+
+
+    void rewind(Time delta) override
+    {
+        kill();
+    }
+
+
+    Sprite& sprite()
+    {
+        return sprite_;
+    }
+
+private:
+    Time timer_ = 0;
+};
+
+
+
 class Amplifier : public Room
 {
 public:
@@ -73,6 +122,30 @@ public:
     {
         Room::update(delta);
         Room::ready();
+
+        static const auto burst_time = milliseconds(1000);
+
+        burst_timer_ += delta;
+        if (burst_timer_ > burst_time) {
+            burst_timer_ -= burst_time;
+
+            auto segment = [&](Fixnum xoff, Fixnum yoff, bool xflip, bool yflip) {
+                auto p = visual_center();
+                p.x += xoff;
+                p.y += yoff;
+                if (auto e = APP.alloc_entity<AmpAnim>(p)) {
+                    e->sprite().set_flip({xflip, yflip});
+                    APP.effects().push(std::move(e));
+                }
+            };
+
+            segment(Fixnum::from_integer(-16), Fixnum::from_integer(-16), false, false);
+            segment(Fixnum::from_integer(-16), 0.0_fixed, false, true);
+            segment(0.0_fixed, Fixnum::from_integer(-16), true, false);
+            segment(0.0_fixed, 0.0_fixed, true, true);
+
+        }
+
         amplify_adjacent();
     }
 
@@ -208,6 +281,7 @@ public:
 
 protected:
     TileId last_tile_;
+    Time burst_timer_ = 0;
 };
 
 
