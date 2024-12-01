9daa6a6446826f0b30be63588a49300a9d8d996f
Evan Bowman <evan.a.bowman@gmail.com>
2024-12-01 06:56:06 -0500

    add world map icon

--- source/data/overlay_world_map.h
+++ source/data/overlay_world_map.h
@@ -7,9 +7,9 @@
 //	Transparent color : FF,00,FF
 //	+ palette 16 entries, not compressed
 //	+ 504 tiles lz77 compressed
-//	Total size: 32 + 2620 = 2652
+//	Total size: 32 + 2684 = 2716
 //
-//	Time-stamp: 2024-11-29, 14:16:54
+//	Time-stamp: 2024-11-30, 06:36:18
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -18,8 +18,8 @@
 #ifndef GRIT_OVERLAY_WORLD_MAP_H
 #define GRIT_OVERLAY_WORLD_MAP_H
 
-#define overlay_world_mapTilesLen 2620
-extern const unsigned int overlay_world_mapTiles[655];
+#define overlay_world_mapTilesLen 2684
+extern const unsigned int overlay_world_mapTiles[671];
 
 #define overlay_world_mapPalLen 32
 extern const unsigned short overlay_world_mapPal[16];
