f1d26ba7bb78f3b2e03994e7e8562f0c7b6a4355
Evan Bowman <evan.a.bowman@gmail.com>
2024-12-10 11:47:35 -0500

    test coverage for descending sort

--- scripts/test/unittest.lisp
+++ scripts/test/unittest.lisp
@@ -207,6 +207,7 @@
 (begin-test "misc")
 
 (assert-eq (range 0 20) (sort (reverse (range 0 20)) <))
+(assert-eq (reverse (range 0 20)) (sort (range 0 20) >))
 (setq temp 99)
 (assert-eq temp 99)
 (unbind 'temp)
