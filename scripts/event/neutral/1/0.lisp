48840b7bb5ed181ebce7b502d8c5277d49358f9c
Evan Bowman <evan.a.bowman@gmail.com>
2024-12-16 11:40:55 -0500

    more variety in mercenary event

--- scripts/event/neutral/1/0.lisp
+++ scripts/event/neutral/1/0.lisp
@@ -1,6 +1,14 @@
 
-(cond
-  ((equal (faction) 'goblin)
-   (eval-file "scripts/event/neutral/1/0_goblin.lisp"))
-  (true
-   (eval-file "scripts/event/neutral/1/0_human.lisp")))
+(let ((scenarios
+       '("scripts/event/neutral/1/0_goblin.lisp"
+         "scripts/event/neutral/1/0_human.lisp")))
+
+  (when (equal (choice 3) 0)
+    ;; Sometimes, swap the two scenarios for interesting variability. The goblin
+    ;; scenario is still more likely when playing as goblins, but sometimes you
+    ;; get the human scenario.
+    (setq scenarios (reverse scenarios)))
+
+  (eval-file (get scenarios (if (equal (faction) 'goblin)
+                                0
+                                1))))
