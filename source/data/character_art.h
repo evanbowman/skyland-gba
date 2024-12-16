46fdda54de3aa94a0b0b4fcf5fc917bf00552bf4
Evan Bowman <evan.a.bowman@gmail.com>
2024-12-16 09:53:06 -0500

    add goblin warrior monk icon

--- source/data/character_art.h
+++ source/data/character_art.h
@@ -9,7 +9,7 @@
 //	+ 704 tiles Metatiled by 4x4 not compressed
 //	Total size: 32 + 22528 = 22560
 //
-//	Time-stamp: 2024-12-16, 06:45:36
+//	Time-stamp: 2024-12-16, 09:52:41
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
