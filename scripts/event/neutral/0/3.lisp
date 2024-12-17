46b192a9d2d18afb73a73cf04e901d2670996437
Evan Bowman <evan.a.bowman@gmail.com>
2024-12-17 10:55:49 -0500

    adjust probabilities in script

--- scripts/event/neutral/0/3.lisp
+++ scripts/event/neutral/0/3.lisp
@@ -7,7 +7,7 @@
        '("/scripts/event/neutral/0/3_goblin.lisp"
          "/scripts/event/neutral/0/3_human.lisp")))
 
-  (when (equal (chance (if (equal (faction) 'human) 2 5)) 0)
+  (when (equal (chance (if (equal (faction) 'human) 3 5)) 0)
     (setq scenarios (reverse scenarios)))
 
   (eval-file (get scenarios (if (equal (faction) 'goblin)
