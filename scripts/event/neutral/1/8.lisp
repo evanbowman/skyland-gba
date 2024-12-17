45a05c6fe5d2cd2fe9d58a552e0fe7d26d0135c8
Evan Bowman <evan.a.bowman@gmail.com>
2024-12-16 20:49:21 -0500

    adjust explorer event

--- scripts/event/neutral/1/8.lisp
+++ scripts/event/neutral/1/8.lisp
@@ -1,5 +1,5 @@
 
-(dialog "<b:/scripts/data/img/explorer.img.bin>You come across an explorer's balloon, floating gently in the breeze. You adjust your engines to keep pace with it...")
+(dialog (loadstr 9))
 
 (opponent-init -3 'neutral)
 
@@ -9,59 +9,66 @@
 
 (terrain-set (opponent) -3)
 
-(chr-new (opponent) 1 14 'neutral '((icon . 22)))
 
 
 (defn on-converge ()
-  (dialog
-   "<c:explorer:22>Hey there! You know, looks like we're going in the same direction! How about we join up?")
+  (dialog (loadstr 10))
 
   (setq on-dialog-closed
         (lambda ()
-          (dialog "He seems harmless, invite him aboard?")
+          (dialog (loadstr 11))
 
           (dialog-await-binary-q-w/lore "welcome aboard!" "sorry, but no"
-                                        '(("let's chat…" .
-                                           "<c:explorer:22> I'm obsessed with finding new islands! When I find one, I mark it with a signal beacon. That's how you can find islands on your sky chart! Neat huh? <B:0> Anyway, can I come aboard?")))
+                                        (list (cons "let's chat…" (loadstr 12))))
 
           (setq on-dialog-closed '())))
   (setq on-converge nil))
 
 
-(defn on-dialog-accepted ()
-  (let ((temp (chr-slots (player)))
-        (join (lambda (txt)
-                (adventure-log-add 53 '())
-                (dialog txt))))
-    (if temp
-        (progn
-          (setq temp (get temp (choice (length temp))))
-          (chr-new (player) (car temp) (cdr temp) 'neutral '((race . 0) (icon . 22)))
-          (chr-del (opponent) 1 14)
-          (if (or (equal (choice 2) 1) (< (coins) 600))
-              (join "The explorer joined your crew!")
-            (progn
-              (coins-set (- (coins) 600))
-              (join "The explorer joined your crew. Hungry, he ate 600@ of your food supplies!"))))
-      (progn
-        (dialog "Sadly, there's no room...")
-        (defn on-dialog-closed ()
-          (dialog "<c:explorer:22>No room in your castle? Hold on, I've got some supplies, I'll help out...")
-          (defn on-dialog-closed ()
-            (alloc-space 'ladder)
-            (sel-input 'ladder
-                       "Place ladder (1x2):"
-                       (lambda (isle x y)
-                         (sound "build0")
-                         (room-new (player) `(ladder ,x ,y))
-                         (chr-del (opponent) 1 14)
-                         (chr-new (player) x (+ 1 y) 'neutral '((race . 0) (icon . 22)))
-                         (dialog "<c:explorer:22> Thanks! I'll try to help out however I can!")
-                         (defn on-dialog-closed ()
-                           (join "The explorer joined your crew!")
-                           (setq on-dialog-closed nil)
-                           (exit)))))))))
-  (exit))
+(let ((params (list
+               (cons 'icon (cond
+                             ((equal (faction) 'goblin) 36)
+                             (true 22)))
+               (cons 'race (cond
+                             ((equal (faction) 'goblin) 1)
+                             (true 0))))))
+
+  (chr-new (opponent) 1 14 'neutral params)
+
+  (defn on-dialog-accepted ()
+    (let ((temp (chr-slots (player)))
+          (join (lambda (txt)
+                  (adventure-log-add 53 '())
+                  (dialog txt))))
+      (if temp
+          (progn
+            (setq temp (get temp (choice (length temp))))
+            (chr-new (player) (car temp) (cdr temp) 'neutral params)
+            (chr-del (opponent) 1 14)
+            (if (or (equal (choice 2) 1) (< (coins) 600))
+                (join (loadstr 13))
+                (progn
+                  (coins-set (- (coins) 600))
+                  (join (loadstr 14)))))
+          (progn
+            (dialog (loadstr 15))
+            (defn on-dialog-closed ()
+              (dialog (loadstr 16))
+              (defn on-dialog-closed ()
+                (alloc-space 'ladder)
+                (sel-input 'ladder
+                           "Place ladder (1x2):"
+                           (lambda (isle x y)
+                             (sound "build0")
+                             (room-new (player) `(ladder ,x ,y))
+                             (chr-del (opponent) 1 14)
+                             (chr-new (player) x (+ 1 y) 'neutral params)
+                             (dialog (loadstr 17))
+                             (defn on-dialog-closed ()
+                               (join (loadstr 18))
+                               (setq on-dialog-closed nil)
+                               (exit)))))))))
+    (exit)))
 
 
 (defn on-dialog-declined ()
