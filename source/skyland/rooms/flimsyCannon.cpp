66e5a173e097af014552cbeaae81c0fcde612b25
Evan Bowman <evan.a.bowman@gmail.com>
2024-12-18 17:59:03 -0500

    actually add source files for flimsy-cannon

---
+++ source/skyland/rooms/flimsyCannon.cpp
@@ -0,0 +1,151 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2024  Evan Bowman. Some rights reserved.
+//
+// This program is source-available; the source code is provided for educational
+// purposes. All copies of the software must be distributed along with this
+// license document.
+//
+// 1. DEFINITION OF SOFTWARE: The term "Software" refers to SKYLAND,
+// including any updates, modifications, or associated documentation provided by
+// Licensor.
+//
+// 2. DERIVATIVE WORKS: Licensee is permitted to modify the source code.
+//
+// 3. COMMERCIAL USE: Commercial use is not allowed.
+//
+// 4. ATTRIBUTION: Licensee is required to provide attribution to Licensor.
+//
+// 5. INTELLECTUAL PROPERTY RIGHTS: All intellectual property rights in the
+// Software shall remain the property of Licensor. The Licensee does not acquire
+// any rights to the Software except for the limited use rights specified in
+// this Agreement.
+//
+// 6. WARRANTY AND LIABILITY: The Software is provided "as is" without warranty
+// of any kind. Licensor shall not be liable for any damages arising out of or
+// related to the use or inability to use the Software.
+//
+// 7. TERMINATION: This Agreement shall terminate automatically if Licensee
+// breaches any of its terms and conditions. Upon termination, Licensee must
+// cease all use of the Software and destroy all copies.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+#include "flimsyCannon.hpp"
+#include "platform/platform.hpp"
+#include "skyland/alloc_entity.hpp"
+#include "skyland/entity/misc/animatedEffect.hpp"
+#include "skyland/entity/projectile/cannonball.hpp"
+#include "skyland/scene_pool.hpp"
+#include "skyland/skyland.hpp"
+#include "skyland/sound.hpp"
+#include "skyland/tile.hpp"
+
+
+
+namespace skyland
+{
+
+
+
+SHARED_VARIABLE(flimsy_cannon_reload_ms);
+extern SharedVariable cannonball_damage;
+
+
+
+extern Sound cannon_sound;
+
+
+
+void FlimsyCannon::format_description(StringBuffer<512>& buffer)
+{
+    make_format(buffer,
+                SYSTR(description_flimsy_cannon)->c_str(),
+                (1.5_fixed * Fixnum(cannonball_damage)).as_integer(),
+                (1.0_fixed * Fixnum(cannonball_damage)).as_integer(),
+                (0.5_fixed * Fixnum(cannonball_damage)).as_integer());
+}
+
+
+
+FlimsyCannon::FlimsyCannon(Island* parent, const RoomCoord& position)
+    : Weapon(parent, name(), position, 1000 * flimsy_cannon_reload_ms)
+{
+}
+
+
+
+void FlimsyCannon::fire()
+{
+    auto island = other_island();
+
+    Vec2<Fixnum> target;
+
+    auto origin = island->origin();
+    origin.x += Fixnum::from_integer(get_target()->x * 16 + 8);
+    origin.y += Fixnum::from_integer(get_target()->y * 16 + 8);
+    target = origin;
+
+    APP.camera()->shake(4);
+
+    auto start = center();
+
+    // This just makes it a bit less likely for cannonballs to
+    // run into the player's own buildings, especially around
+    // corners.
+    if (is_player_island(island)) {
+        start.x -= 6.0_fixed;
+    } else {
+        start.x += 6.0_fixed;
+    }
+
+    if (not PLATFORM.network_peer().is_connected() and
+        APP.game_mode() not_eq App::GameMode::tutorial) {
+        target = rng::sample<4>(target, rng::critical_state);
+    }
+
+    cannon_sound.play(3);
+
+    auto c = APP.alloc_entity<Cannonball>(start, target, parent(), position());
+    if (c) {
+        if (health() < max_health() / 2) {
+            c->set_strength(0);
+        } else if (health() < ((max_health() * 3) / 4)) {
+            c->set_strength(1);
+        } else {
+            c->set_strength(2);
+        }
+        parent()->projectiles().push(std::move(c));
+    }
+
+    auto e = alloc_entity<AnimatedEffect>(start, 47, 49, milliseconds(100));
+    if (e) {
+        APP.effects().push(std::move(e));
+    }
+}
+
+
+
+Time FlimsyCannon::reload_impl() const
+{
+    return 1000 * flimsy_cannon_reload_ms;
+}
+
+
+
+void FlimsyCannon::render_interior(App* app, TileId buffer[16][16])
+{
+    buffer[position().x][position().y] = Tile::flimsy_cannon_1;
+}
+
+
+
+void FlimsyCannon::render_exterior(App* app, TileId buffer[16][16])
+{
+    buffer[position().x][position().y] = Tile::flimsy_cannon_1;
+}
+
+
+
+} // namespace skyland
---
+++ source/skyland/rooms/flimsyCannon.hpp
@@ -0,0 +1,139 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2024  Evan Bowman. Some rights reserved.
+//
+// This program is source-available; the source code is provided for educational
+// purposes. All copies of the software must be distributed along with this
+// license document.
+//
+// 1. DEFINITION OF SOFTWARE: The term "Software" refers to SKYLAND,
+// including any updates, modifications, or associated documentation provided by
+// Licensor.
+//
+// 2. DERIVATIVE WORKS: Licensee is permitted to modify the source code.
+//
+// 3. COMMERCIAL USE: Commercial use is not allowed.
+//
+// 4. ATTRIBUTION: Licensee is required to provide attribution to Licensor.
+//
+// 5. INTELLECTUAL PROPERTY RIGHTS: All intellectual property rights in the
+// Software shall remain the property of Licensor. The Licensee does not acquire
+// any rights to the Software except for the limited use rights specified in
+// this Agreement.
+//
+// 6. WARRANTY AND LIABILITY: The Software is provided "as is" without warranty
+// of any kind. Licensor shall not be liable for any damages arising out of or
+// related to the use or inability to use the Software.
+//
+// 7. TERMINATION: This Agreement shall terminate automatically if Licensee
+// breaches any of its terms and conditions. Upon termination, Licensee must
+// cease all use of the Software and destroy all copies.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+#pragma once
+
+
+#include "skyland/coins.hpp"
+#include "skyland/sharedVariable.hpp"
+#include "skyland/systemString.hpp"
+#include "weapon.hpp"
+
+
+
+namespace skyland
+{
+
+
+
+class FlimsyCannon final : public Weapon
+{
+public:
+    FlimsyCannon(Island* parent, const RoomCoord& position);
+
+
+    void fire() override;
+    Time reload_impl() const override;
+
+
+    void render_interior(App* app, TileId buffer[16][16]) override;
+    void render_exterior(App* app, TileId buffer[16][16]) override;
+
+
+    static WeaponOrientation weapon_orientation()
+    {
+        return WeaponOrientation::horizontal;
+    }
+
+
+    static void format_description(StringBuffer<512>& buffer);
+
+
+    static Category category()
+    {
+        return Category::weapon;
+    }
+
+
+    static RoomProperties::Bitmask properties()
+    {
+        return RoomProperties::disallow_chimney | RoomProperties::roof_hidden |
+               RoomProperties::goblin_only |
+               RoomProperties::workshop_required |
+               RoomProperties::multiboot_compatible;
+    }
+
+
+    bool description_visible() override
+    {
+        return true;
+    }
+
+
+    static Vec2<u8> size()
+    {
+        return {1, 1};
+    }
+
+
+    static const char* name()
+    {
+        return "flimsy-cannon";
+    }
+
+
+    static SystemString ui_name()
+    {
+        return SystemString::block_flimsy_cannon;
+    }
+
+
+    static ATP atp_value()
+    {
+        return 900.0_atp;
+    }
+
+
+    void plot_walkable_zones(bool matrix[16][16],
+                             BasicCharacter* for_character) override
+    {
+        // one cannot walk through this tile, intentionally do nothing.
+    }
+
+
+    static Icon icon()
+    {
+        return 3992;
+    }
+
+
+    static Icon unsel_icon()
+    {
+        return 3976;
+    }
+
+};
+
+
+} // namespace skyland
