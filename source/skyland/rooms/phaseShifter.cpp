94d643e9f649db9dc96099fa6578f39ef042d8f3
Evan Bowman <evan.a.bowman@gmail.com>
2025-01-08 07:08:24 -0500

    add electric ripple effect when activating phase shifter

--- source/skyland/rooms/phaseShifter.cpp
+++ source/skyland/rooms/phaseShifter.cpp
@@ -45,6 +45,126 @@ namespace skyland
 
 
 
+class PhaseRippleEffectQuarter : public Entity
+{
+public:
+    PhaseRippleEffectQuarter(Platform::DynamicTexturePtr dt,
+                             const Vec2<Fixnum>& pos,
+                             int quarter)
+        : Entity({{}, {}}), dt_(dt), timer_(0), quarter_(quarter)
+    {
+        sprite_.set_texture_index(dt->mapping_index());
+
+        auto p = pos;
+
+        switch (quarter) {
+        case 1:
+            sprite_.set_flip({true, false});
+            p.x += 32.0_fixed;
+            break;
+
+        case 2:
+            sprite_.set_flip({false, true});
+            p.y += 32.0_fixed;
+            break;
+
+        case 3:
+            sprite_.set_flip({true, true});
+            p.x += 32.0_fixed;
+            p.y += 32.0_fixed;
+            break;
+        }
+
+        sprite_.set_position(p);
+
+        dt_->remap(70 * 2);
+    }
+
+
+    void shade(ColorConstant clr)
+    {
+        sprite_.set_mix({clr, 255});
+    }
+
+
+    void update(Time delta) override
+    {
+        timer_ += delta * 2;
+
+        if (timer_ > milliseconds(200)) {
+            timer_ -= milliseconds(200);
+
+
+            if (keyframe_ == 4) {
+                kill();
+            } else {
+                keyframe_++;
+                if (quarter_ == 0) {
+                    dt_->remap((70 + keyframe_) * 2);
+                }
+            }
+        }
+    }
+
+
+    void jump_to_end()
+    {
+        timer_ = milliseconds(200);
+        keyframe_ = 4;
+        dt_->remap((70 + keyframe_) * 2);
+    }
+
+
+    void rewind(Time delta) override
+    {
+        timer_ -= delta * 2;
+
+        if (timer_ < 0) {
+            timer_ += milliseconds(200);
+
+            if (keyframe_ == 0) {
+                kill();
+            } else {
+                if (quarter_ == 0) {
+                    dt_->remap((70 + keyframe_) * 2);
+                }
+                keyframe_--;
+            }
+        }
+    }
+
+
+private:
+    Platform::DynamicTexturePtr dt_;
+    Time timer_ = 0;
+    int quarter_;
+    int keyframe_ = 0;
+};
+
+
+
+void phase_ripple(const Vec2<Fixnum>& pos)
+{
+    auto dt = PLATFORM.make_dynamic_texture();
+    if (dt) {
+        auto p = pos;
+        p.x -= 32.0_fixed;
+        p.y -= 32.0_fixed;
+        auto make_segment = [&](int q) {
+            auto e = APP.alloc_entity<PhaseRippleEffectQuarter>(*dt, p, q);
+            if (e) {
+                e->shade(ColorConstant::electric_blue);
+                return APP.effects().push(std::move(e));
+            }
+        };
+        make_segment(3);
+        make_segment(2);
+        make_segment(1);
+        make_segment(0);
+    }
+}
+
+
 SHARED_VARIABLE(phase_shifter_cooldown_ms);
 SHARED_VARIABLE(phase_shifter_duration_ms);
 
@@ -209,6 +329,8 @@ ScenePtr PhaseShifter::select_impl(const RoomCoord& cursor)
     schedule_repaint();
 
     PLATFORM.speaker().play_sound("enter_phase.raw", 6);
+
+    phase_ripple(visual_center());
 
     time_stream::event::PhaseShifterStateChange e;
     e.x_ = position().x;
