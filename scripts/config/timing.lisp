dd49b19e496189814c145d89269362b86f785be4
Evan Bowman <evan.a.bowman@gmail.com>
2025-01-08 08:46:33 -0500

    hmmmm

--- scripts/config/timing.lisp
+++ scripts/config/timing.lisp
@@ -6,7 +6,7 @@
 (foreach (lambda (kvp) (setvar (second kvp) (first kvp)))
  '((25000 . "decimator_reload_ms")
    (25000 . "drone_bay_reload_ms")
-   (20000 . "phase_shifter_cooldown_ms")
+   (18000 . "phase_shifter_cooldown_ms")
    (10000 . "phase_shifter_duration_ms")
    (18000 . "transporter_reload_ms")
    (7000 . "transporter_goblin_perk_ms")
