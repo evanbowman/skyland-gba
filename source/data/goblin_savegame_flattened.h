3c71c44f334e78efdbc9d5c95916a520b5b5eb71
Evan Bowman <evan.a.bowman@gmail.com>
2025-03-16 16:27:43 -0400

    adjust image

--- source/data/goblin_savegame_flattened.h
+++ source/data/goblin_savegame_flattened.h
@@ -9,7 +9,7 @@
 //	+ 331 tiles lz77 compressed
 //	Total size: 32 + 3124 = 3156
 //
-//	Time-stamp: 2025-03-16, 14:41:14
+//	Time-stamp: 2025-03-16, 16:27:31
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
