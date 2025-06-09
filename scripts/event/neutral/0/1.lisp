5b0565c75fe405f315e687ae7fa9f7b8e18a700d
Evan Bowman <evan.a.bowman@gmail.com>
2025-06-09 08:01:00 -0400

    adjust script

--- scripts/event/neutral/0/1.lisp
+++ scripts/event/neutral/0/1.lisp
@@ -1,5 +1,3 @@
 ;; For the sake of variety, the optional fight event has a few variations.
 
-(if (chance 4)
-    (eval-file "scripts/event/neutral/0/1_2.lisp")
-  (eval-file (format "scripts/event/neutral/0/1_%.lisp" (choice 2))))
+(eval-file (format "scripts/event/neutral/0/1_%.lisp" (choice 3)))
