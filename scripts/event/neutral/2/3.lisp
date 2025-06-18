44d0757ef13b11d281627de4a297a949fd1fc362
Evan Bowman <evan.a.bowman@gmail.com>
2025-06-18 07:05:41 -0400

    add another option to stolen decimator event

--- scripts/event/neutral/2/3.lisp
+++ scripts/event/neutral/2/3.lisp
@@ -24,10 +24,30 @@
 (chr-new (opponent) 2 14 'neutral 0)
 
 
+(set-temp 'dec-cost 1500)
+(set-temp 'dec-discount (/ dec-cost 2))
+
+
+(defn/temp hide-evidence? ()
+  (dialog "<c:captain:7>Look, those goblins are getting closer... <B:0> Tell you what - pay me " (string dec-discount) "@ instead of " (string dec-cost) "@, and help me destroy the evidence that I was ever here. <B:0> They'll assume YOU stole it from them directly. Deal?")
+  (dialog-opts-reset)
+  (dialog-opts-push (format "hide evidence (%@)" dec-discount)
+                    (lambda ()
+                      (setq dec-cost dec-discount)
+                      (on-dialog-accepted)
+                      (push-pending-event (+ 1 (choice 2))
+                                          "/scripts/event/hostile/dec-revenge.lisp")))
+  (dialog-opts-push (format "pay %@…" dec-cost) on-dialog-accepted)
+  (dialog-opts-push "no thanks" on-dialog-declined))
+
+
 (defn on-converge ()
-  (dialog "<c:captain:7> I managed to steal this decimator from some goblins, but they're catching up to me! I know... I could sell you the weapon! I'll install it on your island for @1500...")
+  (dialog "<c:captain:7> I managed to steal this decimator from some goblins, but they're catching up to me! I know... I could sell you the weapon! I'll install it on your island for @" (string dec-cost) "...")
   (setq on-converge nil)
-  (dialog-await-binary-q "Here's 1500@…" "no thanks"))
+  (dialog-opts-reset)
+  (dialog-opts-push (format "here's %@…" dec-cost) on-dialog-accepted)
+  (dialog-opts-push "can I have a discount?" hide-evidence?)
+  (dialog-opts-push "no thanks" on-dialog-declined))
 
 
 (setq on-dialog-declined exit)
@@ -36,9 +56,9 @@
 (defn on-dialog-accepted ()
   (if (bound? 'fut) (unbind 'fut))
 
-  (if (< (coins) 1500)
+  (if (< (coins) dec-cost)
       (progn
-        (dialog "<c:captain:7> Sorry, I went to all this trouble, I really can't sell you this tech for less than @1500. Do you want to salvage some stuff to come up with the funds? I'll check back in in 15 seconds?")
+        (dialog "<c:captain:7> Sorry, I went to all this trouble, I really can't sell you this tech for less than @" (string dec-cost) ". Do you want to salvage some stuff to come up with the funds? I'll check back in in 15 seconds?")
         (dialog-await-y/n)
         (let ((f (this)))
           (defn fut ()
@@ -47,10 +67,10 @@
                   (dialog "<c:captain:7> Seems like you have enough now!")
                   (setq on-dialog-closed f))
               (f))))
-        (setq on-dialog-accepted (lambda () (on-timeout 15000 'fut)))
+        (setq on-dialog-accepted (lambda () (on-timeout 12000 'fut)))
         (setq on-dialog-declined (lambda () (unbind 'fut) (exit))))
     (progn
-      (coins-add -1500)
+      (coins-add (* -1 dec-cost))
 
       ;; We wouldn't want the player to get into a position where there isn't
       ;; enough terrain to place the weapon! The game would get locked up. Just
