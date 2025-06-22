99089d82db1832c7d4681c742c314b39a3a997dc
Evan Bowman <evan.a.bowman@gmail.com>
2025-06-22 18:12:55 -0400

    commit missing files

---
+++ source/skyland/scene/debriefScene.cpp
@@ -0,0 +1,50 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2025 Evan Bowman
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License,
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
+// obtain one at http://mozilla.org/MPL/2.0/. */
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "worldScene.hpp"
+#include "debriefScene.hpp"
+#include "zoneImageScene.hpp"
+#include "boxedDialogScene.hpp"
+#include "script/lisp.hpp"
+
+
+
+namespace skyland
+{
+
+
+
+ScenePtr DebriefScene::update(Time delta)
+{
+    auto var = lisp::get_var("debrief-strs");
+    if (var == L_NIL) {
+        return make_scene<ZoneImageScene>();
+    } else if (not lisp::is_list(var)) {
+        if (APP.is_developer_mode()) {
+            PLATFORM.fatal("debrief-strs is non-list!?");
+        }
+        return make_scene<ZoneImageScene>();
+    } else {
+        auto str = var->cons().car()->string().value();
+        auto dialog = allocate_dynamic<DialogString>("dialog-buffer");
+        while (*str not_eq '\0') {
+            dialog->push_back(*(str++));
+        }
+        lisp::set_var("debrief-strs", var->cons().cdr());
+
+        auto ret = make_scene<BoxedDialogScene>(std::move(dialog));
+        ret->set_next_scene(make_deferred_scene<DebriefScene>());
+        return ret;
+    }
+}
+
+
+
+}
---
+++ source/skyland/scene/debriefScene.hpp
@@ -0,0 +1,38 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2025 Evan Bowman
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License,
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
+// obtain one at http://mozilla.org/MPL/2.0/. */
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+#pragma once
+
+#include "skyland/scene.hpp"
+
+
+
+namespace skyland
+{
+
+
+
+class DebriefScene : public Scene
+{
+public:
+
+    DebriefScene()
+    {
+    }
+
+
+    ScenePtr update(Time delta);
+
+};
+
+
+
+}
