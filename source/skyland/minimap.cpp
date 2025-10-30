6ee2b617fbbd0a2da7d8005da5ee4350c822db20
Evan Bowman <evan.a.bowman@gmail.com>
2025-10-30 18:11:22 -0400

    minimap performance improvements

--- source/skyland/minimap.cpp
+++ source/skyland/minimap.cpp
@@ -291,8 +291,31 @@ void repaint(const Settings& settings)
         for (auto& room : APP.player_island().rooms()) {
             auto pos = room->position();
             auto mt = room->metaclass();
-            for (u8 x = pos.x; x < pos.x + room->size().x; ++x) {
-                for (u8 y = pos.y; y < pos.y + room->size().y; ++y) {
+            auto category = (*mt)->category();
+            const auto sz = room->size();
+
+            u8 base_clr;
+            switch (category) {
+            case Room::Category::wall:
+                if ((*mt)->properties() &
+                    RoomProperties::accepts_ion_damage) {
+                    base_clr = color_el_blue_index;
+                } else {
+                    base_clr = color_gray_index;
+                }
+                break;
+
+            case Room::Category::weapon:
+                base_clr = color_burnt_orange_index;
+                break;
+
+            default:
+                base_clr = color_tan_index;
+                break;
+            }
+
+            for (u8 x = pos.x; x < pos.x + sz.x; ++x) {
+                for (u8 y = pos.y; y < pos.y + sz.y; ++y) {
                     auto set_pixel = [&](int xo, int yo, int v) {
                         pixel_buffer[(x + 1) * 3 + xo][((y - 3) * 3 + yo) - 2] =
                             v;
@@ -311,7 +334,7 @@ void repaint(const Settings& settings)
                         set_pixel(2, 2, color_bright_yellow_index);
                         continue;
                     }
-                    if ((*mt)->category() == Room::Category::weapon and
+                    if (category == Room::Category::weapon and
                         ((settings.weapon_loc_ and
                           *settings.weapon_loc_ == Vec2<u8>{x, y}) or
                          (room->group() not_eq Room::Group::none and
@@ -329,34 +352,17 @@ void repaint(const Settings& settings)
                             weapons.push_back(room.get());
                         }
                     }
+
+                    bool wpn_match = ((settings.weapon_loc_ and
+                                       *settings.weapon_loc_ == Vec2<u8>{x, y}) or
+                                      (room->group() not_eq Room::Group::none and
+                                       settings.group_ and
+                                       room->group() == *settings.group_));
+
                     for (int xx = 0; xx < 3; ++xx) {
                         for (int yy = 0; yy < 3; ++yy) {
-                            u8 clr;
-                            switch ((*mt)->category()) {
-                            case Room::Category::wall:
-                                if ((*mt)->properties() &
-                                    RoomProperties::accepts_ion_damage) {
-                                    clr = color_el_blue_index;
-                                } else {
-                                    clr = color_gray_index;
-                                }
-                                break;
-
-                            case Room::Category::weapon:
-                                clr = color_burnt_orange_index;
-                                break;
-
-                            default:
-                                clr = color_tan_index;
-                                break;
-                            }
-
-                            if (((settings.weapon_loc_ and
-                                  *settings.weapon_loc_ == Vec2<u8>{x, y}) or
-                                 (room->group() not_eq Room::Group::none and
-                                  settings.group_ and
-                                  room->group() == *settings.group_)) and
-                                not(xx == 1 and yy == 1)) {
+                            u8 clr = base_clr;
+                            if (wpn_match and not(xx == 1 and yy == 1)) {
                                 clr = color_white_index;
                             }
 
@@ -388,8 +394,31 @@ void repaint(const Settings& settings)
             for (auto& room : isle.rooms()) {
                 auto pos = room->position();
                 auto mt = room->metaclass();
-                for (u8 x = pos.x; x < pos.x + room->size().x; ++x) {
-                    for (u8 y = pos.y; y < pos.y + room->size().y; ++y) {
+                auto category = (*mt)->category();
+                const auto sz = room->size();
+
+                u8 clr;
+                switch (category) {
+                case Room::Category::wall:
+                    if ((*mt)->properties() &
+                        RoomProperties::accepts_ion_damage) {
+                        clr = color_el_blue_index;
+                    } else {
+                        clr = color_gray_index;
+                    }
+                    break;
+
+                case Room::Category::weapon:
+                    clr = color_burnt_orange_index;
+                    break;
+
+                default:
+                    clr = color_tan_index;
+                    break;
+                }
+
+                for (u8 x = pos.x; x < pos.x + sz.x; ++x) {
+                    for (u8 y = pos.y; y < pos.y + sz.y; ++y) {
                         if (isle.fire_present({x, y})) {
                             auto set_pixel = [&](int xo, int yo, int v) {
                                 pixel_buffer[(x + opp_offset) * 3 + xo - 2]
@@ -408,28 +437,9 @@ void repaint(const Settings& settings)
                             set_pixel(2, 2, color_bright_yellow_index);
                             continue;
                         }
+
                         for (int xx = 0; xx < 3; ++xx) {
                             for (int yy = 0; yy < 3; ++yy) {
-                                u8 clr;
-                                switch ((*mt)->category()) {
-                                case Room::Category::wall:
-                                    if ((*mt)->properties() &
-                                        RoomProperties::accepts_ion_damage) {
-                                        clr = color_el_blue_index;
-                                    } else {
-                                        clr = color_gray_index;
-                                    }
-                                    break;
-
-                                case Room::Category::weapon:
-                                    clr = color_burnt_orange_index;
-                                    break;
-
-                                default:
-                                    clr = color_tan_index;
-                                    break;
-                                }
-
                                 pixel_buffer[(x + opp_offset) * 3 + xx - 2]
                                             [((y - 3) * 3 + yy) - 2] = clr;
                             }
