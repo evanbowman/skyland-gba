5f69e9f738fd461743299dcedb2ce877cbc5937b
Evan Bowman <evan.a.bowman@gmail.com>
2025-11-22 19:29:08 -0500

    change underscore key in text entry keyboard to space

--- source/skyland/scene/textEntryScene.cpp
+++ source/skyland/scene/textEntryScene.cpp
@@ -95,7 +95,7 @@ void TextEntryScene::enter(Scene& prev)
 static const char* keyboard[4] = {"# 1 2 3 4 5 6 7 8 9 0 = ",
                                   "q w e r t y u i o p [ ] ",
                                   "a s d f g h j k l ; ' - ",
-                                  "z x c v b n m , . _ / ? "};
+                                  "z x c v b n m , .   / ? "};
 
 
 ScenePtr TextEntryScene::update(Time delta)
