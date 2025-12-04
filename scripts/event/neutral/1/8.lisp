affa735ea4c375810547ddf88689c9b3f91927d1
Evan Bowman <evan.a.bowman@gmail.com>
2025-12-04 16:32:54 -0500

    update character art, a few scripts

--- scripts/event/neutral/1/8.lisp
+++ scripts/event/neutral/1/8.lisp
@@ -4,6 +4,8 @@
 
 
 (cond
+  ((equal (faction) 'sylph)
+   (eval-file "/scripts/event/neutral/1/8_sylph.lisp"))
   ((equal (faction) 'goblin)
    (eval-file "/scripts/event/neutral/1/8_goblin.lisp"))
   (true
--- source/data/character_art.h
+++ source/data/character_art.h
@@ -3,13 +3,13 @@
 
 //======================================================================
 //
-//	character_art, 128x416@4, 
+//	character_art, 128x448@4, 
 //	Transparent color : FF,00,FF
 //	+ palette 16 entries, not compressed
-//	+ 832 tiles Metatiled by 4x4 not compressed
-//	Total size: 32 + 26624 = 26656
+//	+ 896 tiles Metatiled by 4x4 not compressed
+//	Total size: 32 + 28672 = 28704
 //
-//	Time-stamp: 2025-12-04, 13:08:25
+//	Time-stamp: 2025-12-04, 16:32:31
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -18,8 +18,8 @@
 #ifndef GRIT_CHARACTER_ART_H
 #define GRIT_CHARACTER_ART_H
 
-#define character_artTilesLen 26624
-extern const unsigned int character_artTiles[6656];
+#define character_artTilesLen 28672
+extern const unsigned int character_artTiles[7168];
 
 #define character_artPalLen 32
 extern const unsigned short character_artPal[16];
