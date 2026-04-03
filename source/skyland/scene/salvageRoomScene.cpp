3a66ccb3ab300aab9747f44262438d6b12f3399c
Evan Bowman <evan.a.bowman@gmail.com>
2026-04-03 12:58:53 -0400

    bugfix: preserve powerdown state when rewinding block salvage

--- source/skyland/scene/salvageRoomScene.cpp
+++ source/skyland/scene/salvageRoomScene.cpp
@@ -245,7 +245,9 @@ ScenePtr SalvageRoomScene::update(Time delta)
                         island()->destroy_room(cursor_loc);
 
                         if (is_player_island(island())) {
-                            if (room->is_cold_boot()) {
+                            if (room->is_powered_down()) {
+                                room->set_powerdown(false);
+                            } else if (room->is_cold_boot()) {
                                 room->cold_boot_completed();
                             }
                             if (room->group() not_eq Room::Group::none) {
